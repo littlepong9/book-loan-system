bf360b8bfa0838c44eb55932bed9a7bacefbe56c
littlepong9 <leehan9498@gmail.com>
2023-09-07 16:21:09 +0900

    9월 7일
    bookRepository : save(도서 등록), delete(도서 정보 삭제), update(도서 정보 수정) 추가
    loanRepository : save(대출) 추가
    reservationRepository : save(도서 예약) 추가

--- BookLoanSystem/src/main/java/com/example/book/mapper/book/BookMapper.java
+++ BookLoanSystem/src/main/java/com/example/book/mapper/book/BookMapper.java
@@ -1,7 +1,15 @@
 package com.example.book.mapper.book;
 
+import com.example.book.controller.admin.domain.BookRegForm;
+import com.example.book.controller.admin.domain.BookUpdateForm;
+import com.example.book.domain.vo.BookVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BookMapper {
+    void save(BookVO bookVO);
+
+
+    void update(int no, BookUpdateForm form);
+    void delete(int no);
 }
