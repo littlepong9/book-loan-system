edb2f9d74ce26af3acbd42eb876bbcfa0df6b74f
littlepong9 <leehan9498@gmail.com>
2023-09-07 16:52:02 +0900

    9월 7일
    BookRepository, BookMapper 내용 추가 (도서 검색 결과 리스트, 검색 결과 도서 총 수량)

--- BookLoanSystem/src/main/java/com/example/book/mapper/book/BookMapper.java
+++ BookLoanSystem/src/main/java/com/example/book/mapper/book/BookMapper.java
@@ -5,11 +5,13 @@ import com.example.book.controller.admin.domain.BookUpdateForm;
 import com.example.book.domain.vo.BookVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BookMapper {
     void save(BookVO bookVO);
-
-
+    List<BookVO> findAllBySearch(String field, String keyword, int page, int maxResult, String sortBy, String order);
+    int findCountBySearch(String field, String keyword);
     void update(int no, BookUpdateForm form);
     void delete(int no);
 }
--- BookLoanSystem/src/main/java/com/example/book/repository/book/BookRepository.java
+++ BookLoanSystem/src/main/java/com/example/book/repository/book/BookRepository.java
@@ -1,11 +1,13 @@
 package com.example.book.repository.book;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.example.book.controller.admin.domain.BookUpdateForm;
 import com.example.book.domain.vo.BookVO;
 import com.example.book.mapper.book.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 // 도서 저장소 등록, 수정, 삭제, 조회(단 건, 검색, 검색 결과로 나온 총 도서 수량)
@@ -20,14 +22,13 @@ public class BookRepository {
 
     // 검색 결과 도서 리스트
     public List<BookVO> findAllBySearch(String field, String keyword, int page, int maxResult,String sortBy, String order) {
-
-
-        return null;
+        List<BookVO> bookVOS = bookMapper.findAllBySearch(field, keyword, page, maxResult, sortBy, order);
+        return bookVOS;
     }
 
     // 검색 결과로 나온 총 도서 수량 (view 페이징 시 필요)
     public int findCountBySearch(String field, String keyword){
-        return 0;
+        return bookMapper.findCountBySearch(field, keyword);
     }
 
     // 도서 정보 수정
